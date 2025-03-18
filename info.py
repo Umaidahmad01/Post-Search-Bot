ec33d0a820f425a1803fc9e70915b932097db3b0
Umaid <ahmedumaid707@gmail.com>
2025-03-19 00:16:52 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
-API_ID       = 
-API_HASH     = ""
-BOT_TOKEN    = ""
+API_ID       = 20718334
+API_HASH     = "4e81464b29d79c58d0ad8a0c55ece4a5"
+BOT_TOKEN    = "7403693425:AAHaGlkp-zNNPvNeO62xWqwmsRI5apY0Dcs"
 SESSION      = ""
-DATABASE_URI = ""
-LOG_CHANNEL  = 
-ADMIN        = 
-CHANNEL      = "codeflix_bots"
+DATABASE_URI = "mongodb+srv://probito140:umaid2008@cluster0.1utfc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+LOG_CHANNEL  = -1002078429106
+ADMIN        = 5585016974
+CHANNEL      = "Anime_sub_society"
