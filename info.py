89e8b7fd6c7c50edfd3e4574947b4ce97bac7bfd
Umaid <ahmedumaid707@gmail.com>
2025-03-19 01:53:18 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,7 +1,7 @@
 API_ID       = "20718334"
 API_HASH     = "4e81464b29d79c58d0ad8a0c55ece4a5"
 BOT_TOKEN    = "7403693425:AAHaGlkp-zNNPvNeO62xWqwmsRI5apY0Dcs"
-SESSION      = ""
+SESSION      = "BQGyB4UAfLiBgd36-8qDP5AFYrJzFRCg54tpoQLKfLQBrx6yiwdDAlPE2_1NJEdOFmtsxal4CMcrOOVVMWye-mTJ6woqvZcHdfQAyo4AyVr_Ro-e6Nd9vsBSAUtOCbLHKVOjJ35UUum0bRH5NIvnXlkerwyns0wFsTuMrKmEE4PQ8m2bb3WzMfpOncph59jXm5FGFJxUCIqLrO5K_NXhYzXEHF69aR5rpkxkFXn-jEJOpMVJbRtNoYKGh940cPJ1NvFBHjMD3MpuYU6kk92gmHM5zsxAAcOjAMtDeSgQAbBfXinsgjFVtSGoHoEa_PrTDyrWve6ie3PktsMFreeeYH5PRV8v4wAAAAHZ6WgDAA"
 DATABASE_URI = "mongodb+srv://probito140:umaid2008@cluster0.1utfc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 LOG_CHANNEL  = "-1002078429106"
 ADMIN        = "5585016974"
